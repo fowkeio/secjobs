613418fb32b45f2595a522bd9170389d8e631fbe
fowkeio <guilteixeira@proton.com>
2023-08-10 20:27:18 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/fowkeio/secjobs/config"
 	"github.com/fowkeio/secjobs/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	// Initialize router
 	router.Initialize()
 }
