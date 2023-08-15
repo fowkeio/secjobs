a485a68d594be0d017857dcc64c55b8afca337c1
fowkeio <guilteixeira@proton.com>
2023-08-15 16:28:22 -0300

    feat: add list openings handler

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/fowkeio/secjobs/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func ListOpeninsgHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "GET Openings",
-	})
+func ListOpeninsgHandler(ctx *gin.Context) {
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSuccess(ctx, "list-openings", openings)
+
 }
