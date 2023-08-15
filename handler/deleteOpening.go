ae5d73772dd23c7c2edeb6abe6d5dc752477a9a5
fowkeio <guilteixeira@proton.com>
2023-08-15 16:28:31 -0300

    feat: add delete opening handler

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/fowkeio/secjobs/schemas"
 	"github.com/gin-gonic/gin"
 )
 
-func DeleteOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "DELETE Opening",
-	})
+func DeleteOpeningHandler(ctx *gin.Context) {
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, ErrorParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	// Find opening
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+	// Delete opening
+	if err := db.Delete(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("error deleting opening with id: %s", id))
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
 }
