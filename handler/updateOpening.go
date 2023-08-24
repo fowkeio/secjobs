75459f9d570a80508647962ffd22f3845c708006
fowkeio <guilteixeira@protonmail.com>
2023-08-24 17:37:25 -0300

    feat: add updateOpening docs

--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -7,6 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//	@BasePath	/ap1/v1
+
+// @Summary		Update opening
+// @Description	Update a new secjob opening
+// @Tags			Openings
+// @Accept			json
+// @Produce		json
+// @Param			id	query		string	true	"Opening identification"
+// @Param			opening body UpdateOpeningRequest true "Opening data to update"
+// @Success		200	{object}	UpdateOpeningResponse
+// @Failure		400	{object}	ErrorResponse
+// @Failure		404	{object}	ErrorResponse
+// @Router			/opening [put]
 func UpdateOpeningHandler(ctx *gin.Context) {
 	request := UpdateOpeningRequest{}
 	ctx.BindJSON(&request)
